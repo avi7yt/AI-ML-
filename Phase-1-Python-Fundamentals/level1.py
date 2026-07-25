b41a40c204556798759d385e7f705dcfb7f440f0
Akash <avivishwakarma7yt@gmail.com>
2026-07-26 01:02:11 +0530

    Today I learn Phase-1 Python Fundamental

---
+++ Phase-1-Python-Fundamentals/level1.py
@@ -0,0 +1,67 @@
+# Warm Up (Easy)
+# 1. Hello User
+# Problem Statement: Take a user's name and age and print: 
+# user_name = input("Enter your name: ")
+# user_age = input("Enter your age: ")
+# print(f"Hello {user_name}!")
+# print(f"You are {user_age} years old.")
+
+# 2. Calculator
+'''#Problem Statement- Take two numbers
+    Print
+
+    Addition
+    Subtraction
+    Multiplication
+    Division
+    Floor Division
+    Modulus
+    Power
+'''
+# x = int(input("Enter first number: "))
+# y = int(input("Enter second number: "))
+
+# One important concept here is input function always returns string so here we need to convert it into number.
+# print(f"Addition = {x+y}")
+# print(f"Subtraction = {x-y}")
+# print(f"Multiplication = {x*y}")
+# print(f"Division = {x/y}")
+# print(f"Floor Division = {x//y}")
+# print(f"Remainder = {x%y}")
+# print(f"Exponent = {x%y}")
+
+# 3. Even or Odd
+# number = int(input(f"Enter the number: "))
+# if number % 2 == 0: print(f"Number {number} is even.")
+# else: print(f"Number {number} is odd.")
+
+# 4. Positive Negative Zero
+# user_number = int(input("Enter number: "))
+# if user_number > 0: print(f"Number {user_number} is positive.")
+# elif user_number < 0: print(f"Number {user_number} is negative.")
+# else : print(f"Number {user_number} is zero")
+
+# 5. Largest of Two Numbers
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# if a > b: print(f"{a} is maximum")
+# elif b > a: print(f"{b} is maximum")
+# else : print(f"{a},{b} are equal")
+
+
+# 6. Swap Two Numbers
+# using third variable
+# p = int(input("Enter first number: "))
+# q = int(input("Enter second number: "))
+# r = p
+# p = q
+# q = r
+# print(f"Swaped: {p},{q}")
+
+# # Without using third variable
+# s = int(input("Enter first number: "))
+# t = int(input("Enter second number: "))
+
+# s,t = t,s
+# print(f"Swaped: {s},{t}")
